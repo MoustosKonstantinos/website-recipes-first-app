caa274e78829635c60c30704898fbbc08cd423f6
unknown <ripsin123@hotmail.gr>
2019-07-25 14:33:37 +0300

    updated

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Greek Mousaka',
+      'Indescribably! Just awesome!',
+      'http://www.mygreekdish.com/wp-content/uploads/2013/05/Moussaka-recipe-Traditional-Greek-Moussaka-with-Eggplants-800x530.jpg',
+      [
+        new Ingredient('Ground Meat', 1),
+        new Ingredient('Potatoes', 3),
+        new Ingredient('Zucchini', 2),
+        new Ingredient('Béchamel sauce', 1)
+
+      ]),
+    new Recipe('BIG-FAT Burger',
+      'What else to describe?!',
+      'https://www.seriouseats.com/recipes/images/2015/07/20150728-homemade-whopper-food-lab-35-1500x1125.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Tomatoes', 1),
+        new Ingredient('Onions', 1)
+      ]),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
