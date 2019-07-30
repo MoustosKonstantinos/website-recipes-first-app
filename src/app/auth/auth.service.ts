90356ae7417e7a257282ca9a2d7e46aa574317d4
unknown <ripsin123@hotmail.gr>
2019-07-30 18:59:15 +0300

    updated

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAud8zyJ4SlXMhPyDC_6O8ML4y8B5___sU',
+            {
+            email: email,
+            password: password,
+            returnSecureToken: true
+            }
+        );
+    }
+}
